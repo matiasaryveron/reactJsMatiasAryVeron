08096ee28a52209ff592e274de0e38efbca9db32
Matias Ary Veron <veron-matias@hotmail.com>
2024-03-19 20:28:17 -0300

    ReactMatiasAryVeron

---
+++ vite-project/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+<footer>
+        <p>&copy; Copyright 2024 - Matias Ary Veron</p>
+      </footer>
+    )
+}
+export default Footer
---
+++ vite-project/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return(
+    <header>
+        <h1>Soy un titulo</h1>
+        <nav>
+          <a href="">link1</a>
+          <a href="">link2</a>
+          <a href="">link3</a>
+          <a href="">link4</a>
+        </nav>
+      </header>
+    )
+}
+export default Header
