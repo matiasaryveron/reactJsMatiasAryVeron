c5a88ddb3e8902c49d7fce4ecbe1a4b8ad2c5d28
Matias Ary Veron <veron-matias@hotmail.com>
2024-04-17 13:45:34 -0300

    prentrega-2-matiasaryveron

--- vite-project/src/components/ConsolaContainer.jsx
+++ vite-project/src/components/ConsolaContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import data from "./data.json";
 import { useParams } from "react-router-dom"
 import {Button} from 'antd'
-import Consola from './Consola';
+
 
 function ConsolasContainer(){
   const [product, setProducts] = useState ([])
@@ -26,9 +26,21 @@ miPromesa.finally(() => console.log('fin'));
 
  return (
   <>
-   {product.length !== 0 ? <Consola products={products} />: 
+   {product.length !== 0 ? (
+        product.map((product, index) => (
+          <div key={index}>
+            <h2>{product.name}</h2>
+            <img src={product.img} alt="" />
+            <p>{product.product}</p>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+            <p>{product.stock > 0 ? 'Stock Disponible' : 'Sin Stock'}</p>
+            <Button>Comprar</Button>
+          </div>
+        ))
+      ) : (
         <div>Esperando productos</div>
-      }
+      )}
   </>
  );
 }
