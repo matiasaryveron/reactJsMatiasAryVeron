1415593a2bb046f3cb9dd171d6acd04ab36e2265
Matias Ary Veron <veron-matias@hotmail.com>
2024-04-17 14:49:22 -0300

    prentrega-2-matiasaryveron

--- vite-project/src/components/CardProduct.jsx
+++ vite-project/src/components/CardProduct.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 function CardProduct({product}) {
   return (
     <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+      <div className="bg-gray-200 rounded-lg shadow-md">
       <img src={product.img} alt={product.product} className="w-full h-48 object-contain" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{product.product}</div>
@@ -13,6 +14,7 @@ function CardProduct({product}) {
       <div className="px-6 py-4">
         <Link to={`/${product.id}`}>Ver mas</Link>
       </div>
+    </div>
     </div>
   );
 }
