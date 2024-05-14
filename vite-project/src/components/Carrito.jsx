df9462d63805d3f186ecc9f5bbe455cb4174e5ac
Matias Ary Veron <veron-matias@hotmail.com>
2024-05-14 10:26:42 -0300

    Entrega

--- vite-project/src/components/Carrito.jsx
+++ vite-project/src/components/Carrito.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {contexto} from '../contexto.jsx'
+import { contexto } from '../contexto.jsx';
 import Swal from 'sweetalert2';
 
 function Carrito() {
@@ -14,46 +14,34 @@ function Carrito() {
   }
 
   return (
-
     <div>
       <h2 className="text-gray-800 text-3xl font-bold text-center mb-4">MI CARRITO</h2>
-      <div>
-        {carrito.map((item) => {
-       return(
+      {carrito.length === 0 ? (
+        <h3 className="text-gray-800 text-xl font-semibold text-center mb-4 p-4 border border-gray-300 rounded-md">CARRITO VACÍO</h3>
+      ) : (
+        <div>
+          {carrito.map((item) => {
+            return (
               <div key={item.id} className="flex justify-center p-2">
-             <article className="border border-gray-200 p-4 rounded-md shadow-md flex items-center">
-              <img src={item.img} className="w-10 h-10 mr-2" alt={item.name} />
-              <div>
-              <h3 className="text-sm font-semibold">{item.name}</h3>
-                <p>Cantidad: {item.cantidad}</p>
-                <p className="text-gray-600 text-xs">Precio: {item.price} {item.currency}</p>
+                <article className="border border-gray-200 p-4 rounded-md shadow-md flex items-center">
+                  <img src={item.img} className="w-10 h-10 mr-2" alt={item.name} />
+                  <div>
+                    <h3 className="text-sm font-semibold">{item.name}</h3>
+                    <p>Cantidad: {item.cantidad}</p>
+                    <p className="text-gray-600 text-xs">Precio: {item.price} {item.currency}</p>
+                  </div>
+                </article>
               </div>
-              </article>
-</div>
-      )
-    })}
+            );
+          })}
+          <form className="mx-auto max-w-sm" onSubmit={handleSubmit}>
+            <h2 className="text-gray-800 text-2xl font-bold mb-4 text-center">Formulario de Envío</h2>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded block mx-auto">Finalizar Compra</button>
+          </form>
+        </div>
+      )}
     </div>
-    <form className="mx-auto max-w-sm" onSubmit={handleSubmit}>
-  <h2 className="text-gray-800 text-2xl font-bold mb-4 text-center">Formulario de Envio</h2>
-  <div className="mb-4">
-    <input type="text" placeholder="Nombre" className="mb-2 p-2 border border-gray-300 rounded-md bg-gray-100 w-full" required />
-  </div>
-  <div className="mb-4">
-    <input type="text" placeholder="Apellido" className="mb-2 p-2 border border-gray-300 rounded-md bg-gray-100 w-full" required />
-  </div>
-  <div className="mb-4">
-    <input type="text" placeholder="Direccion" className="mb-2 p-2 border border-gray-300 rounded-md bg-gray-100 w-full" required />
-  </div>
-  <div className="mb-4">
-    <input type="email" placeholder="Email" className="mb-2 p-2 border border-gray-300 rounded-md bg-gray-100 w-full" required />
-  </div>
-  <div className="mb-4">
-    <input type="tel" placeholder="Número de Celular" title="Debe tener 10 dígitos" className="mb-2 p-2 border border-gray-300 rounded-md bg-gray-100 w-full" required />
-  </div>
-  <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded block mx-auto">Finalizar Compra</button>
-</form>
-
-    </div>
-  )
+  );
 }
+
 export default Carrito;
--- vite-project/src/contexto.jsx
+++ vite-project/src/contexto.jsx
@@ -1,46 +1,48 @@
 import { createContext, useState } from "react";
 
-export const contexto = createContext() ;
+export const contexto = createContext();
 const Provider = contexto.Provider;
 
-function CarritoProvider (props){
-  const [cantCarrito, setCantCarrito] = useState(0)
-  const [precioTotal, setPrecioTotal] = useState(0)
-  const [carrito, setCarrito] = useState([])
+function CarritoProvider(props) {
+  const [cantCarrito, setCantCarrito] = useState(0);
+  const [precioTotal, setPrecioTotal] = useState(0);
+  const [carrito, setCarrito] = useState([]);
 
+  const agregarAlCarrito = (cant, item) => {
+    setCantCarrito(cantCarrito + cant);
 
+    const productoExistente = carrito.find((producto) => producto.id === item.id);
 
-const agregarAlCarrito = (cant, item) =>{
-  console.log(cant, item)
-  setCantCarrito(cantCarrito + cant)
-  const copia = carrito.slice(0)
-const productoConCantidad = item
-productoConCantidad.cantidad = cant
+    if (productoExistente) {
+      const carritoActualizado = carrito.map((producto) =>
+        producto.id === item.id
+          ? { ...producto, cantidad: producto.cantidad + cant }
+          : producto
+      );
+      setCarrito(carritoActualizado);
+    } else {
+      const productoConCantidad = { ...item, cantidad: cant };
+      setCarrito([...carrito, productoConCantidad]);
+    }
+  };
 
-  copia.push(productoConCantidad)
-  setCarrito(copia)
-}
+  const borrarDelCarrito = (id) => {};
 
-const borrarDelCarrito = (id) => {}
+  const vaciarCarrito = () => {
+    setCantCarrito(0);
+    setCarrito([]);
+  };
 
-const vaciarCarrito =() => {
-  setCantCarrito(0)
-  setCarrito([])
-}
-
-const valorActual ={
+  const valorActual = {
     carrito: carrito,
     cantCarrito: cantCarrito,
     precioTotal: precioTotal,
-    agregarAlCarrito : agregarAlCarrito,
-    borrarDelCarrito : borrarDelCarrito,
-    vaciarCarrito : vaciarCarrito
-}
-  return (
-<Provider value={valorActual}>
-{props.children}
-</Provider>
-    )
+    agregarAlCarrito: agregarAlCarrito,
+    borrarDelCarrito: borrarDelCarrito,
+    vaciarCarrito: vaciarCarrito,
+  };
+
+  return <Provider value={valorActual}>{props.children}</Provider>;
 }
 
-export default CarritoProvider
+export default CarritoProvider;
